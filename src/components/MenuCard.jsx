e010f34c88479c4d5bf0830aa958e031ba8f6dfb
İbrahim Berk Bulğan <berkbulgan@gmail.com>
2023-09-07 13:43:24 +0300

    Logic change

--- src/components/MenuCard.jsx
+++ src/components/MenuCard.jsx
@@ -1,42 +1,50 @@
 import PropTypes from "prop-types";
-import { useState } from "react";
 import { motion } from "framer-motion";
 import classes from "./MenuCard.module.scss";
 import CloseIcon from "./UI/CloseIcon";
 import MenuItem from "./UI/MenuItem";
 
 const MenuCard = (props) => {
-  const [isCardOpen, setIsCardOpen] = useState(false);
+  const animateSettings = {
+    bottom:
+      props.activeMenu.get() === props.cardIndex
+        ? 0
+        : props.activeMenu.get() !== false &&
+          props.activeMenu.get() < props.cardIndex
+        ? (props.cardIndex - 1) * 48 + "px"
+        : props.cardIndex * 48 + "px",
+    height: props.activeMenu.get() === props.cardIndex ? "100%" : "48px",
+    overflow: props.activeMenu.get() === props.cardIndex ? "auto" : "hidden",
+  };
   return (
     <motion.div
-      className={`${classes.card} ${isCardOpen ? classes.card__open : ""}`}
+      className={classes.card}
       style={{
         backgroundColor: props.category.background_color,
         color: props.category.color,
-        zIndex: isCardOpen ? 0 : 10,
+        zIndex: props.activeMenu.get() === props.cardIndex ? 0 : 1,
       }}
-      animate={{
-        bottom: isCardOpen ? 0 : props.cardIndex * 60 + "px",
-        height: isCardOpen ? "100%" : "60px",
-        overflow: isCardOpen ? "auto" : "hidden",
-      }}
+      animate={animateSettings}
       transition={{
-        delay: 0,
+        delay: props.activeMenu.get() === props.cardIndex ? 0.3 : 0,
         duration: 0.3,
       }}
     >
       <div
         className={classes.card__header}
         onClick={() => {
-          setIsCardOpen(!isCardOpen);
-          props.setAnyActive(!props.anyActive);
+          if (props.activeMenu.get() !== props.cardIndex) {
+            props.activeMenu.set(props.cardIndex);
+          } else {
+            props.activeMenu.set(false);
+          }
         }}
       >
         <h2 className={classes.card__title}>{props.category.name}</h2>
         <CloseIcon
           svgClass={`${classes.card__closeIcon}`}
           svgFill={props.category.color}
-          openStatus={isCardOpen}
+          openStatus={props.activeMenu.get() === props.cardIndex}
         />
       </div>
       <div className={classes.card__body}>
@@ -53,6 +61,5 @@ export default MenuCard;
 MenuCard.propTypes = {
   category: PropTypes.object.isRequired,
   cardIndex: PropTypes.number.isRequired,
-  anyActive: PropTypes.bool.isRequired,
-  setAnyActive: PropTypes.func.isRequired,
+  activeMenu: PropTypes.object.isRequired,
 };
--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -2,24 +2,23 @@ import menu from "../assets/menu.json";
 import MenuCard from "../components/MenuCard";
 import AppBackground from "../components/AppBackground";
 
-import { useState } from "react";
+import { useHookstate } from "@hookstate/core";
 import classes from "./Index.module.scss";
 
 function App() {
-  const [menuItems] = useState(Array.from(menu).reverse());
-  const [anyActive, setAnyActive] = useState(false);
+  const menuItems = Array.from(menu).reverse();
+  const activeMenu = useHookstate(false);
   const menuLength = menuItems.length;
   return (
     <div className={classes.main}>
-      <AppBackground height={60 * menuLength} />
+      <AppBackground height={48 * (menuLength - 1)} />
       {menuItems.map((item, index) => {
         return (
           <MenuCard
             key={index}
             category={item}
             cardIndex={index}
-            anyActive={anyActive}
-            setAnyActive={setAnyActive}
+            activeMenu={activeMenu}
           />
         );
       })}
