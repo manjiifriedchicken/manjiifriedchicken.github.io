c9b3cccb033b18b3321669c7f93ef9f612052aee
İbrahim Berk Bulğan <berkbulgan@gmail.com>
2023-12-22 12:58:40 +0300

    hotjar

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import Index from "./pages/Index.jsx";
 import ReactGA from "react-ga4";
+import Hotjar from '@hotjar/browser';
 import "./main.scss";
 import {
   createBrowserRouter,
@@ -10,6 +11,11 @@ import {
   Route,
 } from "react-router-dom";
 
+
+const siteId = 3803867;
+const hotjarVersion = 6;
+
+Hotjar.init(siteId, hotjarVersion);
 ReactGA.initialize("G-6Z1ZX2KNHS");
 ReactGA.send({ hitType: "pageview", page: window.location.pathname, title: "Manjii" });
 
